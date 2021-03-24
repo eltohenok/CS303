ee50164a783105a7a37fa6d8369fab330d3463e6
eltohenok <79168610+eltohenok@users.noreply.github.com>
2021-03-23 18:08:25 -0700

    Update app.js

--- labW1D1/app.js
+++ labW1D1/app.js
@@ -2,35 +2,44 @@
 /* eslint-disable no-unused-vars */
 "use strict";
 
-// function pow(x, n) {
-// 	return x ** n;
-// }
+function pow(x, n) {
+	return x ** n;
+}
 
-// function add2(n1, n2) {
-// 	return n1 + n2;
-// }
+function add2(n1, n2) {
+	return n1 + n2;
+}
 
-// function add(...args) {
-// 	let count = arguments.length;
-// 	let sum = 0;
-// 	for (let i=0; i<count; i++) {
-// 		sum += arguments[i];		
-// 	}
+function add(arr) {
+	let count = arr.length;
+	let sum = 0;
+	for (let i=0; i<count; i++) {
+		sum += arr[i];		
+	}
 	
-// 	return sum;
-// }
+	return sum;
+}
 
-// /*
-// 	Input: grades is an array of integers
-// 	Output: compute average of grades and report letter grade as follows:
-// 	90 - 100 A
-// 	80 - 89  B
-// 	70 - 79  C
-// 	60 - 69  D
-// 	0 - 59   NC
-// */
-// function computeGrade(grades) {
-// 	return "???";
-// }
-console.log("hello")
+/*
+	Input: grades is an array of integers
+	Output: compute average of grades and report letter grade as follows:
+	90 - 100 A
+	80 - 89  B
+	70 - 79  C
+	60 - 69  D
+	0 - 59   NC
+*/
+function computeGrade(grades) {
+    if(grades>=90){
+        return "A"
+    }else if(grades>=80){
+        return "B"
+    }else if(grades>=70){
+        return "C"
+    }else if(grades>=60){
+        return "D"
+    }else if(grades<60){
+        return "nc"
+    }
+}
 
