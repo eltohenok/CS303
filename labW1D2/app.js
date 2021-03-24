b5faa1bfd920218c53a63f9d2ad9b74e756c923d
eltohenok <79168610+eltohenok@users.noreply.github.com>
2021-03-23 18:35:01 -0700

    labW1D2

--- labW1D2/app.js
+++ labW1D2/app.js
@@ -3,5 +3,23 @@
 "use strict";
 
 // See tests.js for behavior
-function nameString(obj) {
+function nameString(object) {
+    if (object === undefined) {
+        return "no object passed";
+        } else if (object === null) {
+        return "null argument";
+        } else {
+        if (Object.keys(object).length === 0) {
+        return "object has no properties";
+        } else {
+        let henok = [];
+        let result;
+        for (let key in object) {
+        henok.push(key); //[a,z,m]
+        result = henok.sort().join("."); //[a,m,z]
+        //"a.m.z" 
+        }
+        return result;
+        }
+        }
 }
